a59a88f5262f7f6adbce5ecfdd256874efdffe7e
Aldi setiawan <aldi60051@gmail.com>
2025-10-21 15:15:50 +0700

    Feat: Integration Seedbar

--- resources/js/Layouts/Authenticated/menuItem.jsx
+++ resources/js/Layouts/Authenticated/menuItem.jsx
@@ -1,13 +1,29 @@
 import { Link } from "@inertiajs/react";
 
 export default function MenuItem({ link, icon, text, isActive, method = "get" }) {
-    return(
+    // jika link kosong atau '#', render button disable
+    if (!link || link === '#') {
+        return (
+            <button
+                className="side-link"
+                type='button'
+                onClick={() => {
+                    alert('Comming Soon')
+                }}
+            >
+                {icon}
+                {text}
+            </button>
+        )
+    }
+    return (
         <>
-            <Link 
-                href={link ? route(link) : null } 
-                className={`side-link ${isActive && 'active'}`}
+            <Link
+                href={route(link)}
+                className={`side-link ${isActive ? 'active' : ''}`}
                 method={method}
-                as="button">
+                as="button"
+            >
                 {icon}
                 {text}
             </Link>
